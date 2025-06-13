001b7c6335271e252077c94413f2ca49d0ab559a
akihide-hatano <a.h.19841202@gmail.com>
2025-06-14 07:51:48 +0900

    GamePlayerStatに修正を加え、seederと同じcolumn名に揃えました。

--- app/Models/GamePlayerStat.php
+++ app/Models/GamePlayerStat.php
@@ -12,38 +12,44 @@ class GamePlayerStat extends Model
     protected $fillable = [
         'game_id',
         'player_id',
-        'team_id',      // その試合で所属していたチーム
-        'position_id',  // その試合で守っていたポジション
+        'team_id',
+        'is_starter',
+        'batting_order',
+        'position_id',
 
-        // 打撃成績
+        // 打撃成績（GamePlayerStatSeederに合わせて修正）
+        'plate_appearances', // Seederにはあるがモデルになかったので追加
         'at_bats',
-        'runs',
         'hits',
         'doubles',
         'triples',
         'home_runs',
-        'runs_batted_in',
+        'rbi',                   // ★rbi に修正★
         'stolen_bases',
         'caught_stealing',
-        'walks',
         'strikeouts',
-        'sacrifice_bunts',
-        'sacrifice_flies',
+        'walks',
         'hit_by_pitch',
-        'gidp', // Double Play Grounded Into Double Play (併殺打)
+        'sac_bunts',             // ★sac_bunts に修正★
+        'sac_flies',             // ★sac_flies に修正★
+        'double_plays',          // ★double_plays に修正★ (gidpから)
+        'errors',                // Seederにはあるがモデルになかったので追加
+        'runs_scored',           // ★runs_scored に修正★ (runsから)
 
-        // 投球成績 (このテーブルで直接管理する場合)
+
+        // 投球成績（GamePlayerStatSeederに合わせて修正）
         'innings_pitched',
         'earned_runs',
-        'pitching_strikeouts',
-        'pitching_walks',
+        'runs_allowed',          // Seederにはあるがモデルになかったので追加
+        'hits_allowed',          // Seederにはあるがモデルになかったので追加
         'home_runs_allowed',
+        'walks_allowed',         // ★walks_allowed に修正★ (pitching_walksから)
+        'strikeouts_pitched',    // ★strikeouts_pitched に修正★ (pitching_strikeoutsから)
         'pitches_thrown',
-        'is_starter', // 先発投手か
-        'is_winner',  // 勝利投手か
-        'is_loser',   // 敗戦投手か
-        'is_saver',   // セーブ投手か
-        'is_holder',  // ホールド投手か
+        'is_winner_pitcher',     // ★is_winner_pitcher に修正★ (is_winnerから)
+        'is_loser_pitcher',      // ★is_loser_pitcher に修正★ (is_loserから)
+        'is_save_pitcher',       // ★is_save_pitcher に修正★ (is_saverから)
+        'is_hold_pitcher',       // ★is_hold_pitcher に修正★ (is_holderから)
     ];
 
     /**
