40f42a8421f545d6ab2cd536972d52496110025e
akihide-hatano <a.h.19841202@gmail.com>
2025-06-16 08:31:54 +0900

    web.phpでplayerのルーティングを追加しました。

--- routes/web.php
+++ routes/web.php
@@ -24,33 +24,29 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // 選手関連のルート (既存のまま)
+    // R: Read (一覧表示) - 通常、最も一般的なルートなので先頭に配置されることが多いですが、
     Route::get('/players', [PlayerController::class, 'index'])->name('players.index');
-    Route::get('/players/{id}', [PlayerController::class, 'show'])->name('players.show');
-    Route::get('/players/create', [PlayerController::class, 'create'])->name('players.create');
-
-    // ★★★ チーム関連のルート (CRUD 全て手動定義 - 順序とパラメータ名を修正) ★★★
-
     // C: Create (新規作成フォーム表示)
-    Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
-
-    // C: Create (データ保存処理)
-    Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
-
-    // U: Update (編集フォーム表示) - /teams/{team} より先に定義
-    Route::get('/teams/{team}/edit', [TeamController::class, 'edit'])->name('teams.edit'); // ★ {id} から {team} に変更 ★
-
+    Route::get('/players/create', [PlayerController::class, 'create'])->name('players.create');
+    // C: Create (データ保存処理) - POST メソッドを使用
+    Route::post('/players', [PlayerController::class, 'store'])->name('players.store');
+    // U: Update (編集フォーム表示)
+    Route::get('/players/{player}/edit', [PlayerController::class, 'edit'])->name('players.edit'); // ★ 追加/修正 ★
     // U: Update (データ更新処理) - PATCH メソッドを使用
-    Route::patch('/teams/{team}', [TeamController::class, 'update'])->name('teams.update'); // ★ {id} から {team} に変更 ★
-
+    Route::patch('/players/{player}', [PlayerController::class, 'update'])->name('players.update'); // ★ 追加 ★
     // D: Delete (データ削除処理) - DELETE メソッドを使用
-    Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy'); // ★ {id} から {team} に変更 ★
-
+    Route::delete('/players/{player}', [PlayerController::class, 'destroy'])->name('players.destroy'); // ★ 追加 ★
     // R: Read (詳細表示) - 最も一般的な動的ルートなので、他の具体的な動的ルートの後に定義
-    Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show'); // ★ {id} から {team} に変更 ★
+    Route::get('/players/{player}', [PlayerController::class, 'show'])->name('players.show'); // ★ {id} から {player} に変更 ★
 
-    // R: Read (一覧表示) - 最も一般的なルートなので、他の具体的なルートの後に定義しても良いですが、
-    // 通常はルートグループの先頭付近に配置されます。
+
+    // チーム関連のルート (既存のまま。これは正しく並んでいます)
+    Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
+    Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
+    Route::get('/teams/{team}/edit', [TeamController::class, 'edit'])->name('teams.edit');
+    Route::patch('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
+    Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
+    Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
     Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
 
 
